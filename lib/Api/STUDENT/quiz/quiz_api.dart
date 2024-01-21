198f4f4d4d3e062926295f5cd0320b7e5045875c
ForhadCU <kashem.tipo@gmail.com>
2024-01-21 21:24:57 +0600

    Up

--- lib/Api/STUDENT/quiz/quiz_api.dart
+++ lib/Api/STUDENT/quiz/quiz_api.dart
@@ -1,7 +1,10 @@
 import 'package:school_management_system/Model/STUDENT/quiz/quiz_info_model.dart';
+import 'package:school_management_system/Model/STUDENT/quiz/quiz_questions_model.dart';
+import 'package:school_management_system/Singletones/app_data.dart';
 
 import '../../../Config/config.dart';
 import '../../../Model/response_model.dart';
+import '../../../Utils/api structure/payloads.dart';
 import '../../../Utils/utils.dart';
 
 class QuizApis {
@@ -19,15 +22,146 @@ class QuizApis {
     ResponseModel res = await CallAPI.getStudentData(
         ApiEndpoint.stuActiveQuizInfo, payLoad, token);
     if (res.statusCode == 200) {
-      kLogger.d(res.body[QuizInfoModel.getParentJsonKey]);
+      // kLogger.d(res.body[QuizInfoModel.getParentJsonKey]);
 
-      kLog("Successfully fetch mGetQuizInfo data");
-      return QuizInfoModel.fromMap(res.body[QuizInfoModel.getParentJsonKey]);
+      if (res.body[QuizInfoModel.getParentJsonKey] != null) {
+        kLog("Successfully fetch mGetQuizInfo data");
+        return QuizInfoModel.fromMap(res.body[QuizInfoModel.getParentJsonKey]);
+      } else {
+        return QuizInfoModel();
+      }
       // return QuizInfoModel();
     } else {
       kLog("mGetQuizInfo status code is: ${res.statusCode}");
       showError("Server failure");
       return QuizInfoModel();
+    }
+  }
+
+  static Future<bool> mSaveQuizAnswerSilently(
+    Map<String, dynamic> payLoad,
+    token,
+  ) async {
+    // kLog("$payLoad   $token");
+
+    /// send start Quiz request: Ready button e click korlei
+    bool isSaveAnswer = false;
+
+    ResponseModel res = await CallAPI.postStudentDataSaveQuiz(
+        ApiEndpoint.stuQuizAnswerSilentSave,
+        payLoad,
+        token,
+        AppData.api_access_key);
+    kLogger.d(res.body);
+
+    if (res.statusCode == 200 && res.body['mode'] == "success") {
+      // kLogger.d(res.body);
+
+      kLog("Successfully fetch mSaveQuizAnswerSilently data");
+      isSaveAnswer = true;
+    } else {
+      kLog("mSaveQuizAnswerSilently status code is: ${res.statusCode}");
+      showError("Server failure");
+      isSaveAnswer = false;
+    }
+    return isSaveAnswer;
+  }
+
+  static Future<bool> mSaveQuizAnswerFinalEnd(
+    Map<String, dynamic> payLoad,
+    token,
+  ) async {
+    // kLog("$payLoad   $token");
+
+    /// send start Quiz request: Ready button e click korlei
+    bool isSaveAnswer = false;
+
+    ResponseModel res = await CallAPI.postStudentDataSaveQuizFinallEnd(
+        ApiEndpoint.stuQuizAnswerEndSave,
+        payLoad,
+        token,
+        AppData.api_access_key);
+    kLogger.d(res.body);
+
+    if (res.statusCode == 200 && res.body['mode'] == "success") {
+      // kLogger.d(res.body);
+
+      kLog("Successfully fetch mSaveQuizAnswerFinalEnd data");
+      isSaveAnswer = true;
+    } else {
+      kLog("mSaveQuizAnswerFinalEnd status code is: ${res.statusCode}");
+      showError("Server failure");
+      isSaveAnswer = false;
+    }
+    return isSaveAnswer;
+  }
+
+  static Future<bool> mSaveQuizAnswerEnd(
+    Map<String, dynamic> payLoad,
+    token,
+  ) async {
+    // kLog("$payLoad   $token");
+
+    /// send start Quiz request: Ready button e click korlei
+    bool isSaveAnswer = false;
+
+    ResponseModel res = await CallAPI.postStudentData(
+        ApiEndpoint.stuQuizAnswerEndSave, payLoad, token);
+    kLogger.d(res.body);
+
+    if (res.statusCode == 200 && res.body['mode'] == "success") {
+      // kLogger.d(res.body);
+
+      kLog("Successfully fetch mSaveQuizAnswerSilently data");
+      isSaveAnswer = true;
+    } else {
+      kLog("mSaveQuizAnswerSilently status code is: ${res.statusCode}");
+      showError("Server failure");
+      isSaveAnswer = false;
+    }
+    return isSaveAnswer;
+  }
+
+  static Future<QuizQuestionsModel> mGetQuizQuestions(
+      Map<String, dynamic> payLoad, token) async {
+    // kLog("$payLoad   $token");
+
+    /// send start Quiz request: Ready button e click korlei
+    /* ResponseModel res =
+        await CallAPI.postStudentData(ApiEndpoint.stuQuizStart, payLoad, token);
+    kLogger.d(res.body); */
+    /// get quizQuestion list
+    ResponseModel res = await CallAPI.getStudentData(
+        ApiEndpoint.stuQuizQuestionList, payLoad, token);
+    if (res.statusCode == 200 && res.body['mode'] == "success") {
+      // kLogger.d(res.body);
+
+      kLog("Successfully fetch mGetQuizQuestions data");
+      return QuizQuestionsModel.fromMap(res.body);
+    } else {
+      kLog("mGetQuizQuestions status code is: ${res.statusCode}");
+      showError("Server failure");
+      return QuizQuestionsModel();
+    }
+  }
+
+  static Future<bool> mRegisterToQuiz(
+      Map<String, dynamic> payLoad, token) async {
+    // kLog("$payLoad   $token");
+
+    /// send start Quiz request: Ready button e click korlei
+    ResponseModel res =
+        await CallAPI.postStudentData(ApiEndpoint.stuQuizStart, payLoad, token);
+    kLogger.d(res.body);
+
+    if (res.statusCode == 200 && res.body['mode'] == "success") {
+      kLogger.d(res.body['message']);
+      kLog("Successfully fetch mStartQuiz data");
+      return true;
+    } else {
+      kLog("mStartQuiz status code is: ${res.statusCode}");
+      showError("Server failure");
+      return false;
     }
   }
 }
