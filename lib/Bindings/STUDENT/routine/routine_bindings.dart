0c3d195c9683b188bdf731a4fdf9e07cda87be1c
ForhadCU <kashem.tipo@gmail.com>
2024-01-21 21:25:12 +0600

    Up

--- lib/Bindings/STUDENT/routine/routine_bindings.dart
+++ lib/Bindings/STUDENT/routine/routine_bindings.dart
@@ -5,6 +5,6 @@ import '../../../Controller/student_library.dart';
 class RoutineBindings extends Bindings {
   @override
   void dependencies() {
-    Get.put(RoutineController());
+    Get.put(StuRoutineController());
   }
 }
